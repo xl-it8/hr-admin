b4b797d2082c1f38e5c535ba3fccd0290693a610
mzli <962729417@qq.com>
2022-07-13 11:49:57 +0800

    '初始化项目'

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import getters from './getters'
+import app from './modules/app'
+import settings from './modules/settings'
+import user from './modules/user'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    app,
+    settings,
+    user
+  },
+  // 全局getters 类似计算属性 方便其他组件获取数据
+  getters
+})
+
+export default store
---
+++ src/utils/request.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseUrl: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+export default request
